0e58d1777ba79aa351fa358d6ccce59c9a68a525
hristijankostadinoski <hristijan@2coders.com>
2025-03-19 21:51:02 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import MyClassComponent from "./examples/lecture_1/components/classComponent/classComponent"
+import MyFirstComponent from "./examples/lecture_1/components/myFirstComponent"
+
+function App() {
+    return (
+        <div>
+            <MyFirstComponent />
+            <MyClassComponent />
+        </div>
+    )
+}
+
+export default App
---
+++ src/examples/lecture_1/components/myFirstComponent.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./button/button"
+
+function MyFirstComponent() {
+    return (
+        <div>
+            <h1>My First Component</h1>
+            <p>This is my first React Component!</p>
+            <Button />
+        </div>
+    )
+}
+
+export default MyFirstComponent
