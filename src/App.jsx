908be1e07519d4fd600b721cf314f40c54ebb42f
Nihil96 <kostadinoskihristijan@yahoo.com>
2025-04-30 20:50:27 +0200

    example of using redux-thunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import "./App.css";
-import Auth from "./examples/lecture_10/components/auth";
-import Counter from "./examples/lecture_10/components/counter";
-import Cart from "./exercises/exercise_4/components/cart";
-import ProductList from "./exercises/exercise_4/components/productList";
+import UsersList from "./examples/lecture_11/components/usersList";
 
 function App() {
   return (
     <>
-      {/* Example 1 */}
-      {/* <Counter /> */}
-
-      {/* Example 2 */}
-      {/* <Auth /> */}
-
-      {/* Exercise 4 */}
-      <h2>Shoping Cart with Redux</h2>
-      <ProductList />
-      <Cart />
+      <UsersList />
     </>
   );
 }
